5441c9b1c663844e639390d93203ed0c793896e7
msohaaib <msohaib968505@gmail.com>
2024-07-06 12:52:22 +0500

    for loop completed

---
+++ 05_Itration/one.js
@@ -0,0 +1,26 @@
+// for loop
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+    
+// }
+
+
+// break and continue
+// for (let i = 1; i <= 20; i++) {
+//     if (i ==5) {
+//         console.log("5 detected");
+//         break;
+//     }
+//     console.log(`the value of i is ${i}`);
+    
+// }
+
+
+for (let i = 1; i <= 20; i++) {
+    if (i ==5) {
+        console.log("5 detected");
+        continue;
+    }
+    console.log(`the value of i is ${i}`);
+    
+}
