86de8ace4df9e81bd108bfbd6ce7a48cd2a6bfb4
msohaaib <msohaib968505@gmail.com>
2024-07-06 12:55:56 +0500

    updated

--- 05_Itration/one.js
+++ 05_Itration/one.js
@@ -16,11 +16,11 @@
 // }
 
 
-for (let i = 1; i <= 20; i++) {
-    if (i ==5) {
-        console.log("5 detected");
-        continue;
-    }
-    console.log(`the value of i is ${i}`);
+// for (let i = 1; i <= 20; i++) {
+//     if (i ==5) {
+//         console.log("5 detected");
+//         continue;
+//     }
+//     console.log(`the value of i is ${i}`);
     
-}
+// }
