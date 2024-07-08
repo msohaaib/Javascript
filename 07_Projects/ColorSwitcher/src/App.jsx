78442443fe8adad14eb306962f7ad435eef50305
msohaaib <msohaib968505@gmail.com>
2024-07-08 20:39:26 +0500

    updated

---
+++ 07_Projects/ColorSwitcher/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+  const buttons = document.querySelectorAll('.btn')
+  const body = document.querySelector("body")
+  buttons.forEach( (button) =>{
+    button.addEventListener('click' , function(e){
+      if (e.target.id === "Gray"){
+        body.style.backgroundColor= "gray"
+      }
+    })
+  } )
+
+  return (
+    <>
+      <div className='flex flex-row gap-14 justify-center mt-48'>
+        <div id='Gray' className='btn h-9 w-9 border rounded-full bg-gray-600 text-white p-10 flex justify-center items-center'>Gray</div>
+        <div id='Green' className='btn h-9 w-9 border rounded-full bg-green-600 text-white p-10 flex justify-center items-center'>Green</div>
+        <div id='Blue' className='btn h-9 w-9 border rounded-full bg-blue-600 text-white p-10 flex justify-center items-center'>Blue</div>
+        <div id='Red' className='btn h-9 w-9 border rounded-full bg-red-600 text-white p-10 flex justify-center items-center'>Red</div>
+        <div id='Black' className='btn h-9 w-9 border rounded-full bg-black text-white p-10 flex justify-center items-center'>Black</div>
+      </div>
+        <h2 className='text-center my-10 text-2xl'>Press any button to change the backgroud color</h2>
+    </>
+  )
+}
+
+export default App
