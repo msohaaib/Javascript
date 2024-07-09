68af0552a2e09920e6e54736da07e1e75fbb5615
msohaaib <msohaib968505@gmail.com>
2024-07-09 11:14:36 +0500

    updated

--- 07_Projects/ColorSwitcher/src/App.jsx
+++ 07_Projects/ColorSwitcher/src/App.jsx
@@ -7,21 +7,20 @@ function App() {
   const body = document.querySelector("body")
   buttons.forEach( (button) =>{
     button.addEventListener('click' , function(e){
-      button.style.cursor = "pointer"
       if (e.target.id === "Gray"){
         body.style.backgroundColor= e.target.id
       }
       else if (e.target.id === "Green"){
-        body.style.backgroundColor = "green"
+        body.style.backgroundColor = e.target.id
       }
       else if (e.target.id === "Blue"){
-        body.style.backgroundColor = "blue"
+        body.style.backgroundColor = e.target.id
       }
       else if (e.target.id === "Red"){
-        body.style.backgroundColor = "red"
+        body.style.backgroundColor = e.target.id
       }
       else if (e.target.id === "Black"){
-        body.style.backgroundColor = "black"
+        body.style.backgroundColor = e.target.id
         body.style.color = "white"
       }
       else{
@@ -33,11 +32,11 @@ function App() {
   return (
     <>
       <div className='flex flex-row gap-14 justify-center mt-48'>
-        <div id='Gray' className='btn h-9 w-9 border rounded-full bg-gray-600 text-white p-10 flex justify-center items-center'>Gray</div>
-        <div id='Green' className='btn h-9 w-9 border rounded-full bg-green-600 text-white p-10 flex justify-center items-center'>Green</div>
-        <div id='Blue' className='btn h-9 w-9 border rounded-full bg-blue-600 text-white p-10 flex justify-center items-center'>Blue</div>
-        <div id='Red' className='btn h-9 w-9 border rounded-full bg-red-600 text-white p-10 flex justify-center items-center'>Red</div>
-        <div id='Black' className='btn h-9 w-9 border rounded-full bg-black text-white p-10 flex justify-center items-center'>Black</div>
+        <div id='Gray' className='btn h-9 w-9 border cursor-pointer rounded-full bg-gray-600 text-white p-10 flex justify-center items-center'>Gray</div>
+        <div id='Green' className='btn h-9 w-9 border cursor-pointer rounded-full bg-green-600 text-white p-10 flex justify-center items-center'>Green</div>
+        <div id='Blue' className='btn h-9 w-9 border cursor-pointer rounded-full bg-blue-600 text-white p-10 flex justify-center items-center'>Blue</div>
+        <div id='Red' className='btn h-9 w-9 border cursor-pointer rounded-full bg-red-600 text-white p-10 flex justify-center items-center'>Red</div>
+        <div id='Black' className='btn h-9 w-9 border cursor-pointer rounded-full bg-black text-white p-10 flex justify-center items-center'>Black</div>
       </div>
         <h2 className='text-center my-10 text-2xl'>Press any button to change the backgroud color</h2>
     </>
