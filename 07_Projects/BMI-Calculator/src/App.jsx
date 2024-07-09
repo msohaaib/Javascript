e65415007245069e8daaa9a7fed153c65c7de7fc
msohaaib <msohaib968505@gmail.com>
2024-07-09 19:06:24 +0500

    updated

--- 07_Projects/BMI-Calculator/src/App.jsx
+++ 07_Projects/BMI-Calculator/src/App.jsx
@@ -6,13 +6,18 @@ function App() {
 
   return (
     <>
-      <div>
-        <h1>BMI - Calculator</h1>
-        <form action="">
-          <label htmlFor="height">Height in cm : </label>
-          <input type="number" name="Weight" id="height" />
-          <label htmlFor="Weight">Weight in KG : </label>
-          <input type="number" name="Weight" id="weight" />
+      <div className='flex flex-col justify-center text-center my-44'>
+        <h1 className='text-3xl my-10'>BMI - Calculator</h1>
+        <form>
+          <div className='flex flex-row gap-4 justify-center my-10 items-center'>
+          <label htmlFor="height" className='text-xl mx-5'>Height in Cm : </label>
+          <input type="number" name="Weight" id="height" className='border h-8 px-4 py-6 border-slate-300 rounded-full focus:border-slate-500' />
+          </div>
+          <div className='flex flex-row gap-4 justify-center items-center'>
+          <label htmlFor="Weight" className='text-xl mx-5'>Weight in KG : </label>
+          <input type="number" name="Weight" id="weight" className='border h-8 px-4 py-6 border-slate-300 rounded-full' />
+          </div>
+          <button className='mt-14 border px-6 py-3 rounded-full text-white bg-slate-600'>Calculate</button>
         </form>
       </div>
     </>
