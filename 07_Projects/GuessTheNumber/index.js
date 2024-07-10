e7b0e79bda39aea50dee5464e3919e24f262f2ba
msohaaib <msohaib968505@gmail.com>
2024-07-10 18:01:49 +0500

    updated

---
+++ 07_Projects/GuessTheNumber/index.js
@@ -0,0 +1,30 @@
+const randomNumber = parseInt(Math.random()*100 +1);
+const submit = document.querySelector("#subt");
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const startOver = document.querySelector('.resultParas');
+
+
+const p = document.createElement('p')
+
+let prevGuess = []
+let numGuesses = 1
+
+let playGame = true
+
+function validateGuess(guess){
+    
+}
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+    
+}
+
+function displayMessage(message){
+
+}
